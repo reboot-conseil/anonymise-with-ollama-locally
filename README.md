b174afd07d0e2884b5d54f4bde42fc2cf127d725
Oscar Dewasmes <oscar.dewasmes@reboot-conseil.com>
2024-08-02 11:08:50 +0200

    :tada: first commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # anonymise-with-ollama-locally
 Code snipet to remove PII from a text using llama3.1 with ollama locally
+
+# Prerequisites
+- ollama installed
+
+https://ollama.com/download
+- `llama3` model downloaded  
+```sh
+ollama pull llama3
+```
+
+# Install
+```sh
+git clone https://github.com/reboot-conseil/anonymise-with-ollama-locally
+cd anonymise-with-ollama-locally
+python3.12 -m venv .venv
+```
+Enter virtual environment:
+- On windows : `.venv/Script/Activate.ps1`
+- On Linux : `source .venv/bin/activate`
+
+Install requirements
+```sh
+pip install -r requirements.txt
+```
+
+# Run demonstration
+Demonstration in jupyter notebook
+
+# Run script
+```sh
+python main.py --input <input_file_path>
+```
+
+```sh
+usage: main.py [-h] --input INPUT [--output OUTPUT] [-v]
+
+options:
+  -h, --help       show this help message and exit
+  --input INPUT    Path to input text file
+  --output OUTPUT  Optional. Path to output text file
+  -v, --verbose    Optional. Verbose mode
+```
---
+++ main.py
@@ -0,0 +1,47 @@
+from langchain_community.llms import Ollama
+import argparse
+import os
+
+
+def anonymise_text(text: str) -> str:
+    assert isinstance(text, str)
+
+    llm = Ollama(model="llama3")
+
+    prompt = """
+    In the following text, replace names with <NAME>, phone numbers with <PHONE_NUMBER>, and addresses with <ADDRESS>.
+    """
+
+    return llm.invoke(prompt + text)
+
+
+if __name__ == "__main__":
+    # argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", type=str, required=True, help="Path to input text file")
+    parser.add_argument("--output", type=str, required=False, default=None, help="Optional. Path to output text file")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Optional. Verbose mode")
+    args = parser.parse_args()
+
+    assert isinstance(args.input, str)
+    assert os.path.exists(args.input)
+    assert os.path.isfile(args.input)
+
+    # open input file
+    with open(args.input, "r") as f:
+        data = f.read()
+
+    # anonymise text
+    anonymised_data = anonymise_text(data)
+
+    # save anonymised text
+    output_path = args.output
+    if output_path is None:
+        output_path = args.input + ".anonymised"
+
+    with open(output_path, "w") as f:
+        f.write(anonymised_data)
+
+    # print output
+    if args.verbose:
+        print(anonymised_data)
